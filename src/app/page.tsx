2497f0e194125670bb1b5c26fe1e5c7026e60442
Angad_Dhiman <angaddhiman10@gmail.com>
2025-01-26 13:27:06 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      {" "}
+      <h1 className="font-bold text-blue-400">Welcome to the world NEXT!!</h1>
+    </div>
+  );
+}
