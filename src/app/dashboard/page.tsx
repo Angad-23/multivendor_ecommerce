881450025edf4e89577970581cef4eb96375fd4c
Angad_Dhiman <angaddhiman10@gmail.com>
2025-02-11 00:14:17 +0530

    11-02-2025 v1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,20 @@
-export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+// Next.js
+import { currentUser } from "@clerk/nextjs/server";
+
+// Clerk
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  // Retrieve the current user information
+  const user = await currentUser();
+
+  // If user role is not defined or is USER, redirect to the home page
+  if (!user?.privateMetadata?.role || user?.privateMetadata.role === "USER")
+    redirect("/");
+
+  // If user role is ADMIN, redirect to the admin dashboard
+  if (user.privateMetadata.role === "ADMIN") redirect("/dashboard/admin");
+
+  // If user role is SELLER, redirect to the seller dashboard
+  if (user.privateMetadata.role === "SELLER") redirect("/dashboard/seller");
 }
